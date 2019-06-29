076005bb8f021f5538e2318e7d7aca4800cf5e97
Prasanth Kolachina <prasanth.kolachina@cse.gu.se>
2019-06-29 12:17:27 +0200

    add more files to gitignore; change wget to curl in download script

--- wat-iitb-enhi/init.sh
+++ wat-iitb-enhi/init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-wget http://www.cfilt.iitb.ac.in/iitb_parallel/iitb_corpus_download/README
-wget http://www.cfilt.iitb.ac.in/iitb_parallel/iitb_corpus_download/dev_test.tgz
-wget http://www.cfilt.iitb.ac.in/iitb_parallel/iitb_corpus_download/parallel.tgz
-wget http://www.cfilt.iitb.ac.in/iitb_parallel/iitb_corpus_download/monolingual.hi.tgz
+curl --remote-name-all  http://www.cfilt.iitb.ac.in/iitb_parallel/iitb_corpus_download/README
+curl --remote-name-all  http://www.cfilt.iitb.ac.in/iitb_parallel/iitb_corpus_download/dev_test.tgz
+curl --remote-name-all  http://www.cfilt.iitb.ac.in/iitb_parallel/iitb_corpus_download/parallel.tgz
+curl --remote-name-all  http://www.cfilt.iitb.ac.in/iitb_parallel/iitb_corpus_download/monolingual.hi.tgz
 
 tar -xvzf dev_test.tgz
 tar -xvzf parallel.tgz
 tar -xvzf monolingual.hi.tgz
 
+rm dev_test.tgz
+rm parallel.tgz
+rm monolingual.hi.tgz
 
